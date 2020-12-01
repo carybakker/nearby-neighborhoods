45b65e7764db8cb2ddc370700fedb26f35f61491
Cary A. Bakker <cary@scientificode.com>
2020-12-01 00:50:15 -0500

    Incremental update to Swift4 / XC10.1

--- swift/nearby-neighborhoods/nearby-neighborhoodsTests/LocationTest.swift
+++ swift/nearby-neighborhoods/nearby-neighborhoodsTests/LocationTest.swift
@@ -11,7 +11,7 @@ class LocationTest : XCTestCase {
   func testDistanceTo() {
     let sf = Location(latitude: 37.7577627,longitude: -122.4727051)
     let nyc = Location(latitude: 40.6976633,longitude: -74.1201054)
-    XCTAssertEqualWithAccuracy(4125, sf.distance(toLocation: nyc), accuracy: 1.0)
+    XCTAssertEqual(4125, sf.distance(toLocation: nyc), accuracy: 1.0)
   }
     
 }
